575a659b0180d56801b1d9453e8b2802a4bfd1bf
Parameshwer <parameshwer0515@gmail.com>
2016-05-28 18:09:29 +0530

    added angular js files

---
+++ public/js/app/app.js
@@ -0,0 +1,24 @@
+var app = angular.module('myApp',['ngRoute']);
+app.config(['$routeProvider', function($routeProvider){
+	$routeProvider.when('/',{
+		title: 'Avens Publishing Group Login',
+		templateUrl: 'partials/login.html'
+	}).when('/Dashboard', {
+		title: 'Avens Publishing Group Dashboard',
+		templateUrl: 'partials/dashboard.html'
+	}).when('/CreateMainCategory', {
+		title: 'Create Main Category',
+		templateUrl: 'partials/main-category.html'
+	}).when('/CreateJournal', {
+		title: 'Create Journal',
+		templateUrl: 'partials/create-journal.html'
+	}).when('/CreateJournalPost', {
+		title: 'Create Journal Post',
+		templateUrl: 'partials/journal-post.html'
+	}).when('/AllPosts', {
+		title: 'All Posts',
+		templateUrl: 'partials/all-posts.html'
+	}).otherwise({
+		redirectTo: '/'
+	});
+}]);
